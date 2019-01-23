23935c7450b5269dfed35217b20e34ff47cf6878
tnakaicode <t.nakai.office@gmail.com>
2019-01-23 15:04:07 +0900

    test bessel

--- Coaxial/Bessel_tm_plot.py
+++ Coaxial/Bessel_tm_plot.py
@@ -19,7 +19,8 @@ sys.path.append(os.path.join('..'))
 
 from src.Unit import convert_SI, convert
 
-def circle_wg_tm_mn (mesh, m=31, n=11, phi=90.0, radi=25.0):
+
+def circle_wg_tm_mn(mesh, m=31, n=11, phi=90.0, radi=25.0):
     x, y = mesh[0], mesh[1]
     r, t = np.sqrt(x**2+y**2), np.arctan(y/x)
     b = radi
@@ -31,13 +32,15 @@ def circle_wg_tm_mn (mesh, m=31, n=11, phi=90.0, radi=25.0):
         e_m = 1
     else:
         e_m = 2
-    
+
     func = np.sqrt(e_m/np.pi)
     func *= 1/np.abs(jvp(m+1, x0_mn))
-    func *= (-jvp(m, x0_mn*r/b, 0) * np.cos(m*t) + m/x0_mn * jvp(m, x0_mn*r/b, 0)/r * np.sin(m*t))
+    func *= (-jvp(m, x0_mn*r/b, 0) * np.cos(m*t) + m /
+             x0_mn * jvp(m, x0_mn*r/b, 0)/r * np.sin(m*t))
     return func
 
-def circle_wg_te_mn (mesh, m=31, n=11, phi=90.0, radi=25.0):
+
+def circle_wg_te_mn(mesh, m=31, n=11, phi=90.0, radi=25.0):
     x, y = mesh[0], mesh[1]
     r, t = np.sqrt(x**2+y**2), np.arctan(y/x)
     b = radi
@@ -49,13 +52,15 @@ def circle_wg_te_mn (mesh, m=31, n=11, phi=90.0, radi=25.0):
         e_m = 1
     else:
         e_m = 2
-    
+
     func = np.sqrt(e_m/np.pi)
-    func *= 1/ (np.sqrt(x1_mn**2-m**2) * np.abs(jvp(m+1, x0_mn)))
-    func *= (-m*jvp(m, x1_mn*r/b, 0)/r * np.cos(m*t) + x1_mn * jvp(m, x1_mn*r/b, 1)/b * np.sin(m*t))
+    func *= 1 / (np.sqrt(x1_mn**2-m**2) * np.abs(jvp(m+1, x0_mn)))
+    func *= (-m*jvp(m, x1_mn*r/b, 0)/r * np.cos(m*t) +
+             x1_mn * jvp(m, x1_mn*r/b, 1)/b * np.sin(m*t))
     return func
 
-def coaxial_wg_tm_mn (mesh, m=31, n=11, phi=90.0, radi_a=0.0, radi_b=25.0):
+
+def coaxial_wg_tm_mn(mesh, m=31, n=11, phi=90.0, radi_a=0.0, radi_b=25.0):
     x, y = mesh[0], mesh[1]
     r, t = np.sqrt(x**2+y**2), np.arctan(y/x)
     a, b = radi_a, radi_b
@@ -67,11 +72,13 @@ def coaxial_wg_tm_mn (mesh, m=31, n=11, phi=90.0, radi_a=0.0, radi_b=25.0):
         e_m = 1
     else:
         e_m = 2
-    
+
     func = np.sqrt(e_m/np.pi)
     func *= 1/np.abs(jvp(m+1, x0_mn))
-    func *= (-jvp(m, x0_mn*r/b, 0) * np.cos(m*t) + m/x0_mn * jvp(m, x0_mn*r/b, 0)/r * np.sin(m*t))
+    func *= (-jvp(m, x0_mn*r/b, 0) * np.cos(m*t) + m /
+             x0_mn * jvp(m, x0_mn*r/b, 0)/r * np.sin(m*t))
     return func
+
 
 if __name__ == "__main__":
     argvs = sys.argv
@@ -97,18 +104,34 @@ if __name__ == "__main__":
     m, n = opt.mn
     a, b = opt.radi
 
-    if m == 0:
-        e_m = 1
-    else:
-        e_m = 2
+    pr = np.linspace(10, 100, 1000)
+    m = 5
+    n = 10
+    print(jn_zeros(m, n)[-1])
+    print(jnp_zeros(m, n)[-1])
+    print(yn_zeros(m, n)[-1])
+    print(ynp_zeros(m, n)[-1])
 
-    x0_i = jn_zeros(m, n)[-1]
-    x1_i = jnp_zeros(m, n)[-1]
-    print (x0_i, x1_i)
+    x0_mn_1 = jn_zeros(m, 5)[-1]
+    x1_mn_1 = jnp_zeros(m, 5)[-1]
+    x0_mn_2 = jn_zeros(m, 7)[-1]
+    x1_mn_2 = jnp_zeros(m, 7)[-1]
+
+    d_1 = jvp(m, pr, 0)*yvp(m, x0_mn_1, 0) - yvp(m, pr, 0)*jvp(m, x0_mn_1, 0)
+    d_2 = jvp(m, pr, 0)*yvp(m, x0_mn_2, 0) - yvp(m, pr, 0)*jvp(m, x0_mn_2, 0)
+
+    plt.figure()
+    plt.plot(pr, d_1)
+    plt.plot(pr, d_2)
+    plt.grid()
     
     plt.figure()
-    plt.contourf(x, y, circle_wg_tm_mn (mesh))
+    plt.plot(pr, jvp(m, pr, 0)*yvp(m, x0_mn_1, 0))
+    plt.plot(pr, jvp(m, pr, 0)*yvp(m, x0_mn_2, 0))
+    plt.grid()
     
     plt.figure()
-    plt.contourf(x, y, circle_wg_te_mn (mesh))
+    plt.plot(pr, yvp(m, pr, 0)*jvp(m, x0_mn_1, 0))
+    plt.plot(pr, yvp(m, pr, 0)*jvp(m, x0_mn_2, 0))
+    plt.grid()
     plt.show()
