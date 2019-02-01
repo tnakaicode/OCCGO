d385bcd66c559def7ca9697adc28a8900cbd6ced
tnakaicode <t.nakai.office@gmail.com>
2019-02-01 11:10:17 +0900

    def wavefront

--- BeamTracing/ray_trace_wavefront.py
+++ BeamTracing/ray_trace_wavefront.py
@@ -32,11 +32,38 @@ from optparse import OptionParser
 sys.path.append(os.path.join('../'))
 
 
+def wavefront(rxy=[1000, 1000], axs=gp_Ax3()):
+    px = np.linspace(-1, 1, 100) * 10
+    py = np.linspace(-1, 1, 100) * 10
+    mesh = np.meshgrid(px, py)
+
+    rx_0 = curvature(mesh[0], rxy[0], 0)
+    ry_0 = curvature(mesh[1], rxy[1], 0)
+    ph_0 = rx_0 + ry_0
+    phas = surf_spl(*mesh, ph_0)
+
+    trf = gp_Trsf()
+    trf.SetTransformation(axs, gp_Ax3())
+    loc_face = TopLoc_Location(trf)
+    phas.Location(loc_face)
+    return phas
+
+def wavefront_xyz(x, y, z, axs=gp_Ax3()):
+    phas = surf_spl(x, y, z)
+
+    trf = gp_Trsf()
+    trf.SetTransformation(axs, gp_Ax3())
+    loc_face = TopLoc_Location(trf)
+    phas.Location(loc_face)
+    return phas
+
+
 if __name__ == "__main__":
     from src.RayTrace.RaySystem import RaySystem, SurfSystem, OptSystem, Multi_RaySystem
     from src.RayTrace.ray_setup import get_axs, get_deg
     from src.Unit import convert_SI, convert
     from src.geomtory import curvature
+    from src.pyocc.surface import surf_spl
 
     argvs = sys.argv
     parser = OptionParser()
@@ -65,19 +92,31 @@ if __name__ == "__main__":
     print(obj.tar.beam_left.Location())
     print(obj.tar.beam_uppr.Location())
     print(obj.tar.beam_bott.Location())
+
+    rxy = [10, 100]
+
+    px = np.linspace(-1, 1, 100) * 25
+    py = np.linspace(-1, 1, 100) * 25
+    mesh = np.meshgrid(px, py)
+
+    rx_0 = curvature(mesh[0], rxy[0], 0)
+    ry_0 = curvature(mesh[1], rxy[1], 0)
+    ph_0 = wavefront_xyz (*mesh, rx_0 + ry_0, obj.ini.beam)
     
-    ax = obj.tar.Move_Axs(obj.tar.beam, obj.tar.axs, gp_Ax3())
-    ax0 = obj.tar.Move_Axs(obj.tar.beam_rght, obj.tar.axs, gp_Ax3())
-    ax1 = obj.tar.Move_Axs(obj.tar.beam_left, obj.tar.axs, gp_Ax3())
-    ax2 = obj.tar.Move_Axs(obj.tar.beam_uppr, obj.tar.axs, gp_Ax3())
-    ax3 = obj.tar.Move_Axs(obj.tar.beam_bott, obj.tar.axs, gp_Ax3())
-    print (ax.Location())
-    print (ax0.Location())
-    print (ax1.Location())
-    print (ax2.Location())
-    print (ax3.Location())
+    dist = obj.ini.beam.Location().Distance(obj.tar.beam.Location())
+
+    rx_1 = curvature(mesh[0], rxy[0]+dist/2, 0) 
+    ry_1 = curvature(mesh[1], rxy[1]+dist/2, 0) 
+    ph_1 = wavefront_xyz (*mesh, rx_1 + ry_1 + dist/2, obj.ini.beam) 
     
+    rx_r = curvature(mesh[0], rxy[0]+dist, 0) 
+    ry_r = curvature(mesh[1], rxy[1]+dist, 0) 
+    ph_r = wavefront_xyz (*mesh, rx_r + ry_r + dist, obj.ini.beam) 
+
     obj.Display_Shape(["BLUE", "GREEN"])
+    obj.display.DisplayShape(ph_0, color="RED")
+    obj.display.DisplayShape(ph_1, color="RED")
+    obj.display.DisplayShape(ph_r, color="YELLOW")
 
     print(obj.tar.beam.Location())
 
