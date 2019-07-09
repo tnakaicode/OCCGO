11c24c9e82c3672f3c9cfafb3e8967c7733452ae
tnakaicode <t.nakai.office@gmail.com>
2019-07-09 12:28:20 +0900

    President Forest Model

--- MoM/WeightedResidual_female.py
+++ MoM/WeightedResidual_female.py
@@ -1,8 +1,32 @@
+import matplotlib.pyplot as plt
 from pandas import Series
-from matplotlib import pyplot
+from pandas import DataFrame
+from pandas import concat
+
 series = Series.from_csv('daily-total-female-births.csv', header=0)
 print(series.head())
 series.plot()
-pyplot.show()
 
 # Persistence Forecast Model
+
+# create lagged dataset
+values = DataFrame(series.values)
+dataframe = concat([values.shift(1), values], axis=1)
+dataframe.columns = ['t-1', 't+1']
+
+# split into train and test sets
+X = dataframe.values
+train_size = int(len(X) * 0.66)
+train, test = X[1:train_size], X[train_size:]
+train_X, train_y = train[:,0], train[:,1]
+test_X, test_y = test[:,0], test[:,1]
+
+# persistence model
+predictions = [x for x in test_X]
+
+# calculate residuals
+residuals = [test_y[i]-predictions[i] for i in range(len(predictions))]
+residuals = DataFrame(residuals)
+print(residuals.head())
+
+plt.show()
