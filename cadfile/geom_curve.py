a5ce2872278dea7661c67862134d4d70b2f782cc
T.Nakai <t.nakai.office@gmail.com>
2019-07-21 04:55:24 +0900

    geom circle

--- cadfile/geom_curve.py
+++ cadfile/geom_curve.py
@@ -14,10 +14,21 @@ from OCC.gp import gp_Ax1, gp_Ax2, gp_Ax3
 from OCC.gp import gp_Pnt, gp_Vec, gp_Dir
 from OCC.gp import gp_Pln, gp_Trsf, gp_Lin
 from OCC.gp import gp_Pnt2d
+from OCC.gp import gp_Circ
+from OCC.Core.Geom import Geom_BezierCurve, Geom_BSplineCurve
+from OCC.Core.Geom import Geom_Circle
 from OCC.Core.GeomAPI import GeomAPI_PointsToBSpline
-from OCC.Core.Geom2dAPI import Geom2dAPI_PointsToBSpline
 from OCC.Core.TColgp import TColgp_Array1OfPnt, TColgp_Array1OfPnt2d
 from OCC.Core.TColGeom import TColGeom_Array1OfCurve
+
+
+def curvature(px, r=1000, s=0.0, t=10.0):
+    """( x + sx )**2 / 2*rx + tx"""
+    if (r == 0):
+        py = np.zeros_like(px + s) + t
+    else:
+        py = (px + s)**2 / (2*r) + t
+    return py
 
 
 def get_airfol_data(filename="./dae51.dat"):
@@ -60,6 +71,16 @@ class Airfoil (object):
     def __init__(self, url):
         self.display, self.start_display, self.add_menu, self.add_function_to_menu = init_display()
         self.url = url
+    
+
+    def gen_circle(self):
+        px = np.linspace(0, 1000, 100)
+        py = curvature (px, r=1000, s=500, t=100)
+        for i, x in enumerate(px):
+            pnt = gp_Pnt(0, 0, x)
+            ax2 = gp_Ax2(pnt, gp_Dir(1, 0, 0))
+            crl = Geom_Circle(ax2, py[i])
+            
 
     def get_airfoil(self, name="dae51"):
         filename = self.url + name + ".dat"
@@ -76,21 +97,6 @@ class Airfoil (object):
         geo_spl = GeomAPI_PointsToBSpline(pts)
         return geo_spl
     
-    def get_airfoil_2d(self, name="dae51", ratio=1.0):
-        filename = self.url + name + ".dat"
-        self.upp, self.bot = get_airfol_data(filename)
-        self.upp_spl = self.gen_data_spline_2d(self.upp, ratio)
-        self.bot_spl = self.gen_data_spline_2d(self.bot, ratio)
-    
-    def gen_data_spline_2d(self, dat, ratio=1.0):
-        num = dat.shape[0]
-        pts = TColgp_Array1OfPnt2d(1, num)
-        for i, xyz in enumerate(dat):
-            pnt = gp_Pnt2d(xyz[0], xyz[1])
-            pts.SetValue(i+1, pnt)
-        geo_spl = Geom2dAPI_PointsToBSpline(pts)
-        return geo_spl
-
     def Display(self):
         for i, xyz in enumerate(self.upp):
             pnt = gp_Pnt(*xyz)
@@ -116,12 +122,9 @@ if __name__ == "__main__":
 
     cfg = json.load(open("airfoil.json", "r"))
     airfilo_url = cfg["url"]
-    airfilo_dat = cfg[opt.name]
-    filename = airfilo_url + airfilo_dat
-    print(filename)
-
+    
     obj = Airfoil(airfilo_url)
-    obj.get_airfoil_2d("dae51", ratio=100)
-    obj.Display()
+    #obj.get_airfoil("dae51")
+    obj.gen_circle()
 
     obj.start_display()
