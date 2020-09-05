bd6243353e651759a86c5ceb6b0c6f2a8d4a6286
tnakaicode <tnakaicode@gmail.com>
2020-09-05 17:32:16 +0900

    Gaussian class

--- PlotGaussian/plot_gauss.py
+++ PlotGaussian/plot_gauss.py
@@ -44,15 +44,47 @@ class GaussianProfile(plot2d):
         px = np.linspace(-1, 1, nx) * lx / 2 - sx
         py = np.linspace(-1, 1, ny) * ly / 2 - sy
         self.mesh = np.meshgrid(px, py)
+
+        self.setup_freq()
         self.ampl = self.setup_ampl()
+        self.phas = self.setup_phas()
+        self.func = self.ampl * np.exp(1j * self.knum * self.phas)
+
+    def setup_freq(self):
+        w_val, w_unit = getline(cfg_txt, 10).split()
+        if w_unit in ["GHz", "kHz", "Hz"]:
+            self.freq = float(w_val)
+            self.knum, self.wave = knum_from_freq(self.freq, w_unit)
+        elif w_unit in ["m", "cm", "mm", "um"]:
+            self.wave = float(w_val)
+            self.knum, self.freq = knum_from_wave(self.wave, w_unit)
+        else:
+            self.freq = 100.0
+            self.knum, self.wave = knum_from_freq(self.freq, "GHz")
 
     def setup_ampl(self):
         val = [float(v) for v in getline(self.cfg_txt, 5).split()]
         wxy = [float(v) for v in getline(self.cfg_txt, 6).split()]
         sxy = [float(v) for v in getline(self.cfg_txt, 7).split()]
         deg = float(getline(self.cfg_txt, 8).split()[0])
-        ampl = gauss_2d(mesh, sxy, wxy, np.rad2deg(deg)) * val[0] + val[1]
+        ampl = gauss_2d(self.mesh, sxy, wxy, np.rad2deg(deg)) * val[0] + val[1]
         return ampl
+
+    def setup_phas(self):
+        p_prof = getline(self.cfg_txt, 11).split()[0]
+        p_txy = [float(v) for v in getline(self.cfg_txt, 12).split()]
+        p_sxy = [float(v) for v in getline(self.cfg_txt, 13).split()]
+        if p_prof == "curvature":
+            phas_x = curvature(self.mesh[0], p_txy[0], p_sxy[0])
+            phas_y = curvature(self.mesh[1], p_txy[1], p_sxy[1])
+        elif p_prof == "tilt":
+            phas_x = self.mesh[0] * np.tan(np.deg2rad(p_txy[0]))
+            phas_y = self.mesh[1] * np.tan(np.deg2rad(p_txy[1]))
+        else:
+            phas_x = np.zeros_like(self.mesh[0])
+            phas_y = np.zeros_like(self.mesh[0])
+        phas = phas_x + phas_y
+        return phas
 
 
 if __name__ == '__main__':
@@ -64,44 +96,5 @@ if __name__ == '__main__':
     print(opt, argc)
 
     cfg_txt = opt.file
-    lx, ly = [float(v) for v in getline(cfg_txt, 1).split()]
-    sx, sy = [float(v) for v in getline(cfg_txt, 2).split()]
-    nx, ny = [int(v) for v in getline(cfg_txt, 3).split()]
-    px = np.linspace(-1, 1, nx) * lx / 2 - sx
-    py = np.linspace(-1, 1, ny) * ly / 2 - sy
-    mesh = np.meshgrid(px, py)
-
-    val, = [float(v) for v in getline(cfg_txt, 5).split()]
-    wxy = [float(v) for v in getline(cfg_txt, 6).split()]
-    sxy = [float(v) for v in getline(cfg_txt, 7).split()]
-    deg, = [float(v) for v in getline(cfg_txt, 8).split()]
-    ampl = gauss_2d(mesh, sxy, wxy, np.rad2deg(deg)) * val + 1.0
-
-    w_val, w_unit = getline(cfg_txt, 10).split()
-    if w_unit in ["GHz", "kHz", "Hz"]:
-        freq = float(w_val)
-        knum, wave = knum_from_freq(freq, w_unit)
-    elif w_unit in ["m", "cm", "mm", "um"]:
-        wave = float(w_val)
-        knum, freq = knum_from_wave(wave, w_unit)
-    else:
-        freq = 100.0
-        knum, wave = knum_from_freq(freq, "GHz")
-
-    p_prof = getline(cfg_txt, 11).split()[0]
-    p_txy = [float(v) for v in getline(cfg_txt, 12).split()]
-    p_sxy = [float(v) for v in getline(cfg_txt, 13).split()]
-    if p_prof == "curvature":
-        phas_x = curvature(mesh[0], p_txy[0], p_sxy[0])
-        phas_y = curvature(mesh[1], p_txy[1], p_sxy[1])
-    elif p_prof == "tilt":
-        phas_x = mesh[0] * np.tan(np.deg2rad(p_txy[0]))
-        phas_y = mesh[1] * np.tan(np.deg2rad(p_txy[1]))
-    else:
-        phas_x = np.zeros_like(ampl)
-        phas_y = np.zeros_like(ampl)
-    phas = phas_x + phas_y
-    func = ampl * np.exp(1j * knum * phas)
-
-    obj = plot2d()
-    obj.contourf_div(mesh, np.abs(func), pngname=obj.tempname)
+    obj = GaussianProfile(cfg_txt)
+    obj.contourf_div(obj.mesh, np.abs(obj.func), pngname=obj.tempname)
