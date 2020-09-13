d1142ec39fc3e4d7f6efae74af0a82772c4aa9e1
tnakaicode <tnakaicode@gmail.com>
2020-09-13 10:04:29 +0900

    fix (error)

--- PlotGaussian/plot_gauss_mat.py
+++ PlotGaussian/plot_gauss_mat.py
@@ -13,7 +13,7 @@ from sympy import im
 sys.path.append(os.path.join("../"))
 from src.base import plot2d, plot3d
 from src.Unit import knum_from_freq, knum_from_wave
-from src.profile import moment, gcf_calc
+from src.profile import moment, gcf_calc, normalize_integrate
 from src.profile import gaussian_func, rot_mesh, get_cov, get_centroid
 from src.geomtory import curvature
 from src.Gaussian import GaussianProfile, gen_noise, copy_file
@@ -23,14 +23,16 @@ logging.getLogger('matplotlib').setLevel(logging.ERROR)
 
 
 def moment_ensable(mesh, func, g_func):
-    #func = func / func.max()
-    #g_func = g_func / g_func.max()
-    fg_func = func * g_func
+    func1 = normalize_integrate(mesh, func)
+    func2 = normalize_integrate(mesh, g_func)
+    #func2 = g_func
+    fg_func = normalize_integrate(mesh, func1 * func2)
+    #fg_func = func * g_func
     sx = moment(mesh, fg_func, [1, 0])
     sy = moment(mesh, fg_func, [0, 1])
-    wx = moment(mesh, fg_func, [2, 0])
-    rt = moment(mesh, fg_func, [1, 1])
-    wy = moment(mesh, fg_func, [0, 2])
+    wx = moment(mesh, fg_func, [2, 0]) * 2
+    rt = moment(mesh, fg_func, [1, 1]) * 2
+    wy = moment(mesh, fg_func, [0, 2]) * 2
     sxy = np.array([sx, sy])
     cov = np.array([
         [wx, rt],
@@ -52,17 +54,21 @@ if __name__ == '__main__':
     ratio = opt.rati
     obj = GaussianProfile(cfg_txt)
 
-    ampl = obj.ampl / obj.ampl.max()
+    ampl = obj.ampl
     mesh_xy = np.stack(obj.mesh, -1)
-    sxy, cov = moment_ensable(obj.mesh, ampl, np.ones_like(ampl))
+    sxy, cov = moment_ensable(obj.mesh, ampl, ampl)
+    g_ampl = multivariate_normal.pdf(mesh_xy, mean=sxy, cov=cov)
     wx, wy = np.sqrt(cov[0, 0]), np.sqrt(cov[1, 1])
-    rot = np.rad2deg(np.arctan(cov[0, 1] / wx / wy))
-    print(sxy, wx, wy, rot)
+    rot = np.rad2deg(np.arccos(cov[0, 1] / wx / wy))
+    gcf = gcf_calc(obj.mesh, ampl, g_ampl)
+    print(sxy, wx, wy, rot, gcf)
     for i in range(100):
         g_ampl = multivariate_normal.pdf(mesh_xy, mean=sxy, cov=cov)
-        g_ampl = g_ampl / g_ampl.max()
         sxy, cov = moment_ensable(obj.mesh, ampl, g_ampl)
-        wx, wy = np.sqrt(cov[0, 0] * 2), np.sqrt(cov[1, 1] * 2)
-        rot = np.rad2deg(np.arctan(cov[0, 1] / wx / wy))
+        wx, wy = np.sqrt(cov[0, 0]), np.sqrt(cov[1, 1])
+        rot = np.rad2deg(np.arccos(cov[0, 1] / wx / wy))
         gcf = gcf_calc(obj.mesh, ampl, g_ampl)
         print(sxy, wx, wy, rot, gcf)
+        wxy, mat = np.linalg.eig(cov)
+        rot = np.rad2deg(np.arctan2(mat[0, 1], mat[1, 1]))
+        print(np.sqrt(wxy), rot)
