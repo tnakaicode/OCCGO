06021b8094516bcc51a67c1ac10e2d557a64dc94
tnakaicode <tnakaicode@gmail.com>
2021-02-12 01:05:08 +0900

    fix

--- cadfile/geom_box_skelton.py
+++ cadfile/geom_box_skelton.py
@@ -1,18 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import sys
+
+sys.path.append(os.path.join("../"))
+from src.base import plotocc
 
 from OCC.Display.SimpleGui import init_display
 from OCC.Core.gp import gp_Ax1, gp_Ax2, gp_Ax3
 from OCC.Core.gp import gp_Pnt, gp_Vec, gp_Dir
 from OCC.Core.gp import gp_Pln, gp_Trsf, gp_Lin
 
-from OCC.Extend.ShapeFactory import make_box
-# from OCCUtils.Construct import make_box
+#from OCC.Extend.ShapeFactory import make_box
+from OCCUtils.Construct import make_box
 
 if __name__ == "__main__":
     display, start_display, add_menu, add_function_to_menu = init_display()
 
     pnt = gp_Pnt()
+    plotocc.make_EllipWire(None)
     display.DisplayShape(pnt)
     display.DisplayShape(make_box(pnt, 100, 100, 100), transparency=0.01)
     display.DisplayShape(make_box(gp_Pnt(50, 50, 50), 10, 10, 10))
